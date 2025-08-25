cfcecfe86aada8a97377bcca285c15ed7658c466
kgcemon <kgcemonbd@gmail.com>
2025-08-25 11:25:44 +0600

    updated

--- resources/views/admin/modal/userblockmodal.blade.php
+++ resources/views/admin/modal/userblockmodal.blade.php
@@ -1,54 +1,46 @@
-<div class="modal fade" id="actionModal" tabindex="-1" aria-labelledby="actionModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form action="{{route('users.update')}}" method="POST">
-                @csrf
-                @method('PUT')
-
+<div class="modal fade" id="actionModal" tabindex="-1" role="dialog" aria-labelledby="actionModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <form action="{{route('users.update')}}" method="POST">
+            @csrf
+            <input type="hidden" name="user_id" id="modal_user_id">
+            <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="actionModalLabel">Manage User</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h5 class="modal-title">Update User and Wallet Status</h5>
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span>&times;</span>
+                    </button>
                 </div>
-
                 <div class="modal-body">
-                    <input type="hidden" id="modal_user_id" name="user_id">
-                    <input type="hidden" id="modal_block_status" name="is_block">
-                    <input type="hidden" id="modal_wallet_status" name="wallet">
-
-                    <div class="mb-3">
-                        <label for="modal_user_name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="modal_user_name" name="name" readonly>
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" name="name" id="modal_user_name" class="form-control" readonly>
                     </div>
-
-                    <div class="mb-3">
-                        <label for="modal_user_email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="modal_user_email" name="email" readonly>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" name="email" id="modal_user_email" class="form-control" readonly>
                     </div>
-
-                    <div class="mb-3">
-                        <label for="modal_user_active_wallet" class="form-label">Active Wallet</label>
-                        <input type="text" class="form-control" id="modal_user_active_wallet" name="active_wallet" readonly>
+                    <div class="form-group">
+                        <label>Active Wallet</label>
+                        <input type="text" name="active_wallet" id="modal_user_active_wallet" class="form-control">
                     </div>
-
-                    <div class="mb-3">
-                        <label for="modal_user_deposit_wallet" class="form-label">Deposit Wallet</label>
-                        <input type="text" class="form-control" id="modal_user_deposit_wallet" name="deposit_wallet" readonly>
+                    <div class="form-group">
+                        <label>Deposit Wallet</label>
+                        <input type="text" name="deposit_wallet" id="modal_user_deposit_wallet" class="form-control">
                     </div>
-
-                    <div class="mb-3">
-                        <label class="form-label">Block Status</label>
-                        <select class="form-select" name="block_action" id="modal_block_action">
+                    <div class="form-group">
+                        <label>User Status</label>
+                        <select id="modal_block_status" class="form-control" name="is_block">
                             <option value="0">Unblock</option>
                             <option value="1">Block</option>
                         </select>
                     </div>
                 </div>
-
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Update Status</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
                 </div>
-            </form>
-        </div>
+            </div>
+        </form>
     </div>
 </div>
+
