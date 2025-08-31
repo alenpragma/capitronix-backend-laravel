2d799adb36032ce42f52be7efb451c74f6d3a332
kgcemon <kgcemonbd@gmail.com>
2025-08-31 23:39:47 +0600

    updated

--- app/Http/Controllers/admin/UsersController.php
+++ app/Http/Controllers/admin/UsersController.php
@@ -35,7 +35,7 @@ class UsersController extends Controller
                     break;
             }
 
-            return $query->paginate(10);
+            return $query->orderBy('created_at', 'desc')->paginate(15);
         });
 
         return view('admin.pages.users.index', compact('users'));
